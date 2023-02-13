abb9012b6ceeb8cecce463af8baa0f19f21e6510
Forrest Keller <forrestblackburnkeller@gmail.com>
2023-02-13 12:08:54 -0600

    Change Vice President

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Heading, Icon, Link, Stack } from '@chakra-ui/react';
-import type { NextPage } from 'next';
-import { GiPhotoCamera } from 'react-icons/gi';
-import { GoMailRead } from 'react-icons/go'
-import Layout from '../components/layout';
-import SEO from '../components/seo';
+import { Heading, Icon, Link, Stack } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import { GiPhotoCamera } from "react-icons/gi";
+import { GoMailRead } from "react-icons/go";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const Home: NextPage = () => {
   return (
@@ -11,22 +11,40 @@ const Home: NextPage = () => {
       <SEO />
       <Layout>
         <Stack align="center" spacing={10}>
-          <Stack direction={{ base: "column", md: "row" }} spacing={10} textAlign="center">
+          <Stack
+            direction={{ base: "column", md: "row" }}
+            spacing={10}
+            textAlign="center"
+          >
             <Stack spacing={1}>
-              <Heading fontSize="md" color="secondary">President</Heading>
+              <Heading fontSize="md" color="secondary">
+                President
+              </Heading>
               <Heading fontSize="xl">Forrest Keller</Heading>
             </Stack>
             <Stack spacing={1}>
-              <Heading fontSize="md" color="secondary">Vice President</Heading>
-              <Heading fontSize="xl">William Hinson</Heading>
+              <Heading fontSize="md" color="secondary">
+                Vice President
+              </Heading>
+              <Heading fontSize="xl">Beckett Legore</Heading>
             </Stack>
           </Stack>
           <Stack spacing={5} align="center">
-            <Link variant="social" href={"https://docs.google.com/forms/d/e/1FAIpQLSf0TQf2LS5CiicxndaGTV-q8K7UsYhF1oZXDuT5nYnmCVq4ug/viewform?usp=sf_link"} isExternal>
+            <Link
+              variant="social"
+              href={
+                "https://docs.google.com/forms/d/e/1FAIpQLSf0TQf2LS5CiicxndaGTV-q8K7UsYhF1oZXDuT5nYnmCVq4ug/viewform?usp=sf_link"
+              }
+              isExternal
+            >
               <Icon fontSize="xl" as={GoMailRead} />
               Mailing List
             </Link>
-            <Link variant="social" href={"https://www.instagram.com/tucoffeeclub/"} isExternal>
+            <Link
+              variant="social"
+              href={"https://www.instagram.com/tucoffeeclub/"}
+              isExternal
+            >
               <Icon fontSize="2xl" as={GiPhotoCamera} />
               Instagram
             </Link>
